b1d4f9ea1c58eb5c160a3bd2383cba46e6dd3360
Serek <Serek@DESKTOP-3J8FKQN>
2024-03-24 11:55:26 +0200

    Добавьте файлы проекта.

---
+++ Class work 7/IMath.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+// Інтерфейс IMath
+public interface IMath
+{
+    int Max();
+    int Min();
+    float Avg();
+    bool Search(int valueToSearch);
+}
+
+public class MathArray : IMath
+{
+    private int[] data;
+
+    public MathArray(int[] array)
+    {
+        data = array;
+    }
+
+    public int Max()
+    {
+        int max = data[0];
+        for (int i = 1; i < data.Length; i++)
+        {
+            if (data[i] > max)
+            {
+                max = data[i];
+            }
+        }
+        return max;
+    }
+
+    public int Min()
+    {
+        int min = data[0];
+        for (int i = 1; i < data.Length; i++)
+        {
+            if (data[i] < min)
+            {
+                min = data[i];
+            }
+        }
+        return min;
+    }
+
+    public float Avg()
+    {
+        int sum = 0;
+        foreach (int num in data)
+        {
+            sum += num;
+        }
+        return (float)sum / data.Length;
+    }
+
+    public bool Search(int valueToSearch)
+    {
+        foreach (int num in data)
+        {
+            if (num == valueToSearch)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Class work 7/IOutput.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+// Інтерфейс IOutput
+public interface IOutput
+{
+    void Show();
+    void Show(string info);
+}
+
+public class Array : IOutput
+{
+    private int[] data;
+
+    public Array(int[] array)
+    {
+        data = array;
+    }
+
+    public void Show()
+    {
+        Console.WriteLine("Елементи масиву:");
+        foreach (int item in data)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public void Show(string info)
+    {
+        Console.WriteLine(info);
+        Show();
+    }
+
+}
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        Array array = new Array(arr);
+        array.Show();
+        array.Show("Це масив чисел:");
+
+        Console.WriteLine();
+
+        int[] arr1 = { 1, 3, 5, 7, 9 };
+        MathArray mathArray = new MathArray(arr1);
+
+        Console.WriteLine("Max: " + mathArray.Max());
+        Console.WriteLine("Min: " + mathArray.Min());
+        Console.WriteLine("Avg: " + mathArray.Avg());
+        Console.WriteLine("Find num 5: " + mathArray.Search(5));
+        Console.WriteLine("Find num 10: " + mathArray.Search(10));
+
+        Console.ReadLine();
+    }
+}
